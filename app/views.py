e9d66bd1746bf74e73a3d7b3a696f8e1a2403381
Albert Sobreo <janalbertsobreo@gmail.com>
2020-12-01 21:57:08 +0800

    added backend

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.http.response import JsonResponse
+from django.shortcuts import render, HttpResponse, JsonResponse
 import json
+from .models import Item
 
 def input(request):
     return render(request, '', {})
@@ -9,4 +11,17 @@ def input_process(request):
         try:
             data = json.loads(request.body)
 
-            
+            item = Item()
+            item.item_code = data['item_code']
+            item.item_name = data['item_name']
+
+            item.save()
+
+            return JsonResponse({'message': 'Success'})
+
+def list_items(request):
+    context = {
+        'item': Item.objects.all()
+    }
+
+    return render(request, '', context)
